6f07614b55e7a60d7ef5d5dc92e30b10744d6749
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-10-10 10:18:00 -0700

    cemetery

---
+++ p5image.js
@@ -0,0 +1,69 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+
+
+function draw() {
+   background(7, 12, 92);
+   // moon
+   fill(255);
+  noStroke();
+  circle(350, 100, 100);
+
+ // large hill
+  fill(3, 56, 2);
+  noStroke();
+  circle(500, 400, 700);
+
+  // medium hill
+  fill(16, 66, 7);
+  noStroke();
+  circle(100,500, 600);
+
+  // small hill
+  fill(19, 82, 7);
+  noStroke();
+  circle(400, 500, 500);
+
+
+  // tombstone 1 shine
+  fill(255);
+  noStroke();
+  circle(204, 250, 20);
+  fill(2255);
+  noStroke();
+  rect(194, 250, 20, 20);
+
+  // tombstone 1
+  fill(210);
+  noStroke();
+  circle(200, 250, 20);
+  fill(210);
+  noStroke();
+  rect(190, 250, 20, 20);
+
+  // tombstone 2 shine
+  fill(255);
+  noStroke();
+  circle(304, 350, 20);
+  fill(255);
+  noStroke();
+  rect(294, 350, 20, 20);
+
+  // tombstone 2
+  fill(210);
+  noStroke();
+  circle(300, 350, 20);
+  fill(210);
+  noStroke();
+  rect(290, 350, 20, 20);
+
+  // tombstone 3
+   fill(225);
+  noStroke();
+  circle(295, 150, 10);
+  fill(225);
+  noStroke();
+  rect(290, 150, 10, 10);
+  }
