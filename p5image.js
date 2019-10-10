ebd3e231e29f1cf75e2c406295feda756d8df6b6
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-10-10 11:10:31 -0700

    Update p5image.js

--- p5image.js
+++ p5image.js
@@ -1,28 +1,25 @@
 var img;
-function preload(){
+
+function preload() {
 
   img = loadImage("art-artistic-blue-775907.jpg");
 }
 
-function setup(){
+function setup() {
   createCanvas(600, 600);
 }
 
-
-
-
-
 function draw() {
 
-   image(img, 0, 0);
+  image(img, 0, 0);
 
- fill(3, 82, 252);
+  fill(3, 82, 252);
   rect(100, 100, 200, 400);
 
-  fill (5, 255, 9);
+  fill(5, 255, 9);
   triangle(20, 40, 50, 40, 200, 200);
 
-   fill (5, 255, 9);
+  fill(5, 255, 9);
   triangle(40, 80, 50, 40, 200, 200);
 
 
