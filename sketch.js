64d57336353a245abbde97afc1d3261d74e09e90
tanguiano92 <47006589+tanguiano92@users.noreply.github.com>
2019-10-17 14:11:59 -0700

    css

--- sketch.js
+++ sketch.js
@@ -1,24 +1,18 @@
-var img;
-
-function preload() {
-
-  img = loadImage("images/butterfly.png");
-}
 
 function setup() {
-  //createCanvas(windowWidth, windowHeight);
-  //background(0)
-  //stroke(179, 5, 5);
+  createCanvas(windowWidth, windowHeight);
+  background(0)
+  stroke(179, 5, 5);
 
-   //for (let j = 0; j < 10; j ++){
-   //for (let i = 0; i < 10; i ++){
-  //line (j * 150, 0, windowWidth, i * 150);
-  //}
+  for (let j = 0; j < 10; j ++){
+  for (let i = 0; i < 10; i ++){
+  line (j * 150, 0, windowWidth, i * 150);
+  }
 
-    //for (let j = 0; j < 10; j ++){
-   //for (let i = 0; i < 10; i ++){
-  //line (j * 150, windowWidth, 0, i * 150);
-  //}
+  for (let j = 0; j < 10; j ++){
+   for (let i = 0; i < 10; i ++){
+  line (j * 150, windowWidth, 0, i * 150);
+  }
 
        // Notes for loops&Nested loops:
   //for (let i = 0; i < 10; i ++){
@@ -42,7 +36,6 @@ function setup() {
 
 
 function draw() {
-  image(img, 0, 0);
 
 }
 }
